e7a09c1ee6edc0f52d46e653077b5e69743f4e6f
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-05 11:15:00 +0900

    20231205 11:14

---
+++ docs/data_types.py
@@ -0,0 +1,15 @@
+pass
+type("yugyeongjo") # 문자 type
+# <class 'str'>
+
+type(1997) # 숫자 type
+# <class 'int'> 
+
+name = "yugyeongjo"   # 문자 type
+type(name)
+# <class 'str'>
+
+age = 24 # 숫자 type
+type(age)
+<class 'int'>
+
