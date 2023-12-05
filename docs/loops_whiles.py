e95c4ba2be7a7482a24dbd909c231128778f31a0
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-05 18:03:57 +0900

    add

---
+++ docs/loops_whiles.py
@@ -0,0 +1,26 @@
+# # condition이 True 동안 동작
+# while True :
+#     pass
+
+# 풀기 : 5 구구단 결과를 매번 출력
+
+num_virtual = 0
+# while 정지 1 case without break문 사용
+while True :
+    pass
+    num_virtual = num_virtual + 1
+    print("{} = num_virtual + 1".format(num_virtual))
+    if num_virtual >= 5 : 
+        pass
+        break
+    pass
+
+# while 정지 2 case without break문 사용
+num_virtual = 0
+while num_virtual < 5 :
+    pass
+    num_virtual = num_virtual + 1
+    print("{} = num_virtual + 1".format(num_virtual))
+    pass
+
+print("Programming End!")
