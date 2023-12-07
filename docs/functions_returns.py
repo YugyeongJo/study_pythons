86a523a42f71edc8b04959d0eba6c21c9d332d1d
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-07 15:34:26 +0900

    study

--- docs/functions_returns.py
+++ docs/functions_returns.py
@@ -22,6 +22,7 @@ def print_question_and_answer() :
 
 # print_question_and_answer()
 
+## call by reference
 ## 나오는 값 처리
 
 def add() :
@@ -32,7 +33,7 @@ def add() :
 
 # num_sum = 0
 num_sum = add()      # 2차, 3차 활용을 위해서 해당하는 값을 어떠한 변수에다가 담아서 활용
-print("add() return 결과 : {}".format(num_sum)
+print("add() return 결과 : {}".format(num_sum))
 print(add())
 
 def minus() :
@@ -53,4 +54,31 @@ num_result = multiply()
 print("multiply() 의 결과 : {}".format(num_result))
 
 
+print("--------------------")
 
+#예제
+def return_list() :     
+    list_fruits = ["melon", "apple", "banana", "cherry"]
+    return list_fruits
+print("return_list() return result : {}".format(return_list()))
+
+# list 중에 index로 값 return
+def return_listbyindex() :
+    list_fruits = ["melon", "apple", "banana", "cherry"]
+    return list_fruits[0] 
+print("return_listbyindex() return resul: {}".format(return_listbyindex()))
+
+print("----------------------")
+
+# 반복적인 print 작업을 줄이기 위한 function 만들기
+def score() :
+    my_score = 79
+    my_grade = ''
+    if my_score >= 90 :
+        my_grade = 'A'
+    elif my_score > 80 :
+        my_grade = 'B'
+    else :
+        my_grade = 'C'
+    return my_grade
+print("당신의 학점은 {}입니다.".format(score()))
