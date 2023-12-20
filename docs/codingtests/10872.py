e980d35aba27806ed156357c855eeb56f03fc5b1
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-20 20:21:19 +0900

    add

---
+++ docs/codingtests/10872.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/10872
+
+# 문제
+# 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+
+# 출력
+# 첫째 줄에 N!을 출력한다.
+   
+    
+n = 10
+
+count = 0
+N = []
+
+while count < n :
+    count += 1
+    N.append(count)
+    pass
+print(N)
+
+M = 1
+for i in range(n):
+    M = M * N[i]
+    pass
