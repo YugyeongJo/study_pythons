2c6f649685bcf15f7eea1e74e9a256fa53e8a75d
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-01-03 09:18:04 +0900

    add

--- docs/quests/timestables_function.py
+++ docs/quests/timestables_function.py
@@ -1,3 +1,17 @@
 # - [30, 35, 20] 단 대한 출력
 # - timestables_fors.py -> function 화
 # - option) [30, 35, 20] 부분 사용자 입력, 'q'이면 종료
+
+
+while True :
+    times = input("몇 단을 출력하시겠습니까?")
+    
+    if times == "q":
+        break    
+    
+    num_times = int(times)
+    
+    count = [0,1,2,3,4,5,6,7,8]
+    for i in count:
+        multiply = num_times * (count[i]+1)
+        print("{}*{}={}".format(num_times, (count[i]+1), multiply))
