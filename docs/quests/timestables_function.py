ccc7adc0431432760427533374d4b6b6cb97f984
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-01-03 09:18:45 +0900

    add

--- docs/quests/timestables_function.py
+++ docs/quests/timestables_function.py
@@ -2,16 +2,19 @@
 # - timestables_fors.py -> function 화
 # - option) [30, 35, 20] 부분 사용자 입력, 'q'이면 종료
 
+def calculator ():
+    while True :
+        times = input("몇 단을 출력하시겠습니까?")
+        
+        if times == "q":
+            break    
+        
+        num_times = int(times)
+        
+        count = [0,1,2,3,4,5,6,7,8]
+        for i in count:
+            multiply = num_times * (count[i]+1)
+            print("{}*{}={}".format(num_times, (count[i]+1), multiply))
+    return 0
 
-while True :
-    times = input("몇 단을 출력하시겠습니까?")
-    
-    if times == "q":
-        break    
-    
-    num_times = int(times)
-    
-    count = [0,1,2,3,4,5,6,7,8]
-    for i in count:
-        multiply = num_times * (count[i]+1)
-        print("{}*{}={}".format(num_times, (count[i]+1), multiply))
+calculator ()
