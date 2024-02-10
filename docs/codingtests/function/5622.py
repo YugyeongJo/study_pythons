e1a242ac0312a9861517ba88ae445c9839ffb662
YugyeongJo <yugyeongjo1215@gmail.com>
2024-02-10 22:17:34 +0900

    add codingtest

--- docs/codingtests/function/5622.py
+++ docs/codingtests/function/5622.py
@@ -19,8 +19,16 @@
 
 A = list(input())
 alphabet = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+num = 0
 
-alphabet.index(A)
+def dial(A, alphabet, num):
+    for i in range(len(A)):
+        for j in alphabet:
+            if A[i] in j:
+                num += alphabet.index(j)+3
+                pass
+    return num
+
+answer = dial(A, alphabet, num)
+print(answer)
 pass
-# for value in alphabet:
-#     alphabet.index()
