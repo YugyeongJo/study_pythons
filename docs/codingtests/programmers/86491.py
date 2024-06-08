9e1feaf958592e436b661833e68b8e0de724ba8d
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-06-09 00:46:27 +0900

    UPDATE

--- docs/codingtests/programmers/86491.py
+++ docs/codingtests/programmers/86491.py
@@ -14,8 +14,22 @@
 
 # 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
 
-sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
+sizes = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
 
 def solution(sizes):
     answer = 0
+    horizontal_size = []
+    vertical_size = []
+    for x in sizes:
+        if x[0] > x[1]:
+            horizontal_size.append(x[0])
+            vertical_size.append(x[1])
+        else: 
+            horizontal_size.append(x[1])
+            vertical_size.append(x[0])
+        a = max(horizontal_size)
+        b = max(vertical_size)
+        answer = a*b
     return answer
+
+print(solution(sizes))
