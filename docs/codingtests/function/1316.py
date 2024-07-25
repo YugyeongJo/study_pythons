e8aec90196b64e19efe48ec8bf6cf0d5c85d20f2
YugyeongJo <yugyeongjo1215@gmail.com>
2024-07-25 11:10:20 +0900

    update

---
+++ docs/codingtests/function/1316.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/1316
+
+# 문제
+# 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+
+# 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+
+# 출력
+# 첫째 줄에 그룹 단어의 개수를 출력한다.
+
+# 풀이
+# input 받은 값 별로 그룹단어인지 아닌지 확인 후 그룹단어가 아니라면 n -1
+# 그룹단어인지 판별은 다음 index str이랑 다르다면으로 진행
+
+n = int(input())
+
+def solution(n):
+    for i in range(n):
+        string = input()
+        for j in range(len(string)-1):
+            if string[j] == string[j+1]:
+                pass
+            elif string[j] in string[j+1:]:
+                n -= 1
+                break  
+    return n
+
+print(solution(n))
